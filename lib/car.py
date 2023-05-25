6c4c9215a3bfc9defb6e222027e0d462f0f73238
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-05-25 14:27:40 +0300

    completed assignment

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,15 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self,wheel_size,wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+    def fill_up_tank(self):
+        return "filling up!"    
+
+
+    
